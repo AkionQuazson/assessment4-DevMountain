36dc761c416d9bf8d9ed0a90b56dc7ae20bad1ac
spencer <spencer@homelightarchive.com>
2022-10-08 12:59:35 -0600

    Got fortune, built HTML

--- server/controller.js
+++ server/controller.js
@@ -1,13 +1,34 @@
+let compliments = [
+	"Gee, you're a smart cookie!", 
+	"Cool shirt!", 
+	"Your Javascript skills are stellar."
+];
+let fortunes = [
+	"A faithful friend is a strong defense.", 
+	"A good friendship is often more important than a passionate romance.", 
+	"First think of what you want to do; then do what you have to do.", 
+    "A feather in the hand is better than a bird in the air.", 
+    "Happiness will bring you good luck.", 
+    "Listen to everyone. Ideas come from everywhere.", 
+    "Good to begin well, better to end well."
+];
+
 module.exports = {
 
     getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
-      
         // choose random compliment
         let randomIndex = Math.floor(Math.random() * compliments.length);
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
     }
 
 }
